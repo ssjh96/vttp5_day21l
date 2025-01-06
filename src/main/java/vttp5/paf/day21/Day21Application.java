e6dcbf8799c0de6ac172ae74fe90c25892e5eccd
ssjh96 <ssjh96@outlook.com>
2025-01-06 11:06:42 +0800

    spring

---
+++ src/main/java/vttp5/paf/day21/Day21Application.java
@@ -0,0 +1,13 @@
+package vttp5.paf.day21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day21Application.class, args);
+	}
+
+}
